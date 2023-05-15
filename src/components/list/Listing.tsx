4ed54dde2f2768a20652cc9f587950d6deea5fd2
Seongjun Lee <253eosam@gmail.com>
2023-05-15 18:00:23 +0900

    dev listing

--- src/components/list/Listing.tsx
+++ src/components/list/Listing.tsx
@@ -1,29 +1,91 @@
-import React from 'react'
-import ListTable from './ListingTable';
+import React, { useState } from 'react'
+import ListTable, { IColumn } from './ListingTable'
+
+interface IPost {
+  id: number
+  title: string
+  description: string | null
+  hits: number | null
+  [key: string]: any
+}
 
 const List: React.FC<any> = () => {
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  ];
+  const [checkedArr, setCheckedArr] = useState<number[]>([])
 
-  const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    { field: 'age', headerName: 'Age', type: 'number', width: 90, },
-  ];
+  const rows: IPost[] = [
+    {
+      id: 1,
+      title: 'The Art of War',
+      description: 'A classic treatise on military strategy written by Sun Tzu',
+      hits: 1000,
+    },
+    {
+      id: 2,
+      title: "The Hitchhiker's Guide to the Galaxy",
+      description: 'A comedic science fiction series written by Douglas Adams',
+      hits: 500,
+    },
+    {
+      id: 3,
+      title: 'To Kill a Mockingbird',
+      description: 'A novel by Harper Lee that addresses issues of racism and injustice',
+      hits: 1200,
+    },
+    {
+      id: 4,
+      title: 'The Great Gatsby',
+      description: 'A novel by F. Scott Fitzgerald that explores the decadence and excess of the Roaring Twenties',
+      hits: 800,
+    },
+    {
+      id: 5,
+      title: 'The Lord of the Rings',
+      description: 'An epic high fantasy novel by J.R.R. Tolkien',
+      hits: 1500,
+    },
+    {
+      id: 6,
+      title: '1984',
+      description: 'A dystopian novel by George Orwell that examines the dangers of totalitarianism',
+      hits: 900,
+    },
+    {
+      id: 7,
+      title: 'Pride and Prejudice',
+      description: 'A romantic novel by Jane Austen that satirizes the social norms of 19th-century England',
+      hits: 700,
+    },
+    {
+      id: 8,
+      title: 'The Catcher in the Rye',
+      description: 'A coming-of-age novel by J.D. Salinger that explores themes of alienation and teenage angst',
+      hits: 600,
+    },
+    {
+      id: 9,
+      title: 'The Picture of Dorian Gray',
+      description: 'A novel by Oscar Wilde that explores the themes of beauty, morality, and corruption',
+      hits: 400,
+    },
+    {
+      id: 10,
+      title: 'One Hundred Years of Solitude',
+      description: 'A novel by Gabriel Garcia Marquez that chronicles the history of the fictional town of Macondo',
+      hits: 1100,
+    },
+  ]
+
+  const columns: IColumn[] = [
+    { field: 'checkbox', headerName: 'checkbox', type: 'checkbox', style: { width: 42 } }, // checkbox width is fixed 45px
+    { field: 'id', headerName: 'ID', style: { width: 42, align: 'center' } },
+    { field: 'title', headerName: '제목', type: 'link', style: { width: 3 / 10 } },
+    { field: 'description', headerName: '설명', style: { width: 6 / 10 } },
+    { field: 'hits', headerName: '조회수', type: 'number', style: { width: 1 / 10 } },
+  ]
 
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <ListTable columns={columns} rows={rows} />
+    <div style={{ height: 500, width: '100%' }}>
+      <ListTable columns={columns} rows={rows as any} watchChecked={val => setCheckedArr(val)} />
     </div>
   )
 }
--- src/components/list/ListingTable.tsx
+++ src/components/list/ListingTable.tsx
@@ -1,46 +1,106 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { Box, Checkbox, Link, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { useMemo, useState } from 'react';
 
-interface IColumn {
+export interface IColumn {
   field: string,
-  type: 'text' | 'checkbox',
+  type?: 'string' | 'checkbox' | 'number' | 'link',
   headerName?: string
+  style?: IColumnStyle
 }
 
-const ListTable: React.FC<any> = ({ columns, rows }) => {
+interface IColumnStyle {
+  width?: number
+  align?: 'center',
+  link?: boolean
+}
+
+interface IRow {
+  id: number
+  [key: string]: any
+}
+
+interface IProps {
+  columns: IColumn[]
+  rows: IRow[] 
+  watchChecked?: (checked: number[]) => void
+}
+
+const CHECKBOX_ALL_EVENT_VALUE = -1
+
+const ListTable: React.FC<IProps> = ({ columns, rows, watchChecked }) => {
+  const [checkedArr, setCheckedArr] = useState<number[]>([])
+  const isCheckedAll = useMemo(() => checkedArr.length === rows.length , [checkedArr.length, rows.length])
+  const notifyCheckedWatcher = (id : number, e: React.ChangeEvent<HTMLInputElement>) => {
+    const status = e.target.checked
+    let arr = [...checkedArr]
+    if (id === CHECKBOX_ALL_EVENT_VALUE) {
+      arr = status ? rows.map(({id}) => id) : []
+    }
+    else if (status) arr.push(id)
+    else arr.splice(arr.indexOf(id),1) 
+    setCheckedArr(arr)
+    !!watchChecked && watchChecked(arr)
+  }
 
   const TableHeaderCell = (column: IColumn): any => {
-    const types = {
-      "text": () => {
-        return (<TableCell>{column.headerName ?? columns.field}</TableCell>)
-      },
-      "checkbox": () => {
-        return (<TableCell></TableCell>)
-      },
-    }
+    return <TableCell variant='head' align="center" sx={{ ...column.style, }}>{ column.type === 'checkbox' ? <Checkbox onChange={(event) => notifyCheckedWatcher(CHECKBOX_ALL_EVENT_VALUE, event)} checked={isCheckedAll} /> : (column.headerName ?? column.field)}</TableCell>
+  }
 
-    return types[column.type] ? types[column.type]() : types.text()
+  const TableBodyCell = (row: IRow) => {
+    const mappingHeaders = columns.map(({ field, type, style }) => ({ val: row[field], field, type, style }))
+    const typeCells = {
+      "string": (val: string) => val,
+      "number": (val: number) => (+val).toLocaleString('en'),
+      "link": (val: string) => <Link variant='subtitle1' underline='hover' style={{ cursor: 'pointer'}} >{val}</Link>,
+      "checkbox": (val: boolean) => (<Checkbox onChange={(event) => notifyCheckedWatcher(row.id, event)} checked={checkedArr.includes(row.id)} />),
+    }
+    return (
+      <TableRow>
+        {mappingHeaders.map(item => (
+          <TableCell
+            key={`${row.id}.${item.field}`}
+            align={item.style?.align}
+            sx={item.style}
+          >
+          { (item.type && typeCells[item.type]) ? (typeCells[item.type] as (param: any) => string)(item.val) :  typeCells.string(item.val) }  
+          </TableCell>
+        ))}
+      </TableRow>
+    )
   }
 
   return (
-    <TableContainer>
-      <Table>
-        <TableHead>
-          <TableRow>
+    <Box sx={{ height: '100%' }}>
+      <Paper sx={{ height: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ height: '100%' }}>
+        <Table stickyHeader>
+          <TableHead >
+            <TableRow>
+              {
+                columns.map((col: any) => (
+                  <TableHeaderCell
+                    key={col.field}
+                    {...col}
+                  ></TableHeaderCell>
+                ))
+              }
+            </TableRow>
+          </TableHead>
+          <TableBody>
             {
-              columns.map((col: any) => (
-                <TableHeaderCell
-                  key={col.field}
-                  {...col}
-                ></TableHeaderCell>
+              rows.map((row: any) => (
+                <TableBodyCell
+                  key={row.id}
+                  {...row}
+                >
+                </TableBodyCell>
               ))
             }
-          </TableRow>
-        </TableHead>
-        <TableBody>
-
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableBody>
+        </Table>
+      </TableContainer>
+      </Paper>
+    </Box>
   )
 }
 
