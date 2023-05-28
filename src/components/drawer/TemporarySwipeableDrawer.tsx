fe8f8c1966f2a10742244403eeba7c670303d2c4
Seongjun Lee <253eosam@gmail.com>
2023-05-29 01:10:46 +0900

    husky test -2

--- src/components/drawer/TemporarySwipeableDrawer.tsx
+++ src/components/drawer/TemporarySwipeableDrawer.tsx
@@ -1,38 +1,39 @@
-import { Button, Skeleton, SwipeableDrawer, Typography } from '@mui/material'
-import { styled } from '@mui/material/styles'
-import Box from '@mui/material/Box'
-import { grey } from '@mui/material/colors'
-import { Global } from '@emotion/react'
-import CssBaseline from '@mui/material/CssBaseline'
-import { atom, selector, useRecoilState, useRecoilValue } from 'recoil'
-import useBottomSwiperDrawer from '@src/hooks/useBottomSwiperDrawer'
+import { Button, Skeleton, SwipeableDrawer, Typography } from "@mui/material"
+import { styled } from "@mui/material/styles"
+import Box from "@mui/material/Box"
+import { grey } from "@mui/material/colors"
+import { Global } from "@emotion/react"
+import CssBaseline from "@mui/material/CssBaseline"
+import { atom, selector, useRecoilState, useRecoilValue } from "recoil"
+import useBottomSwiperDrawer from "@src/hooks/useBottomSwiperDrawer"
 
 export const drawerAtom = atom({
-  key: 'drawer.drawerAtom', 
-  default: false, 
+  key: "drawer.drawerAtom",
+  default: false,
 })
 export const drawerComponentAtom = atom({
-  key: 'drawer.drawerComponentAtom',
+  key: "drawer.drawerComponentAtom",
   default: null,
 })
 
 const drawerBleeding = 56
 
 const StyledBox = styled(Box)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'light' ? '#fff' : grey[800],
+  backgroundColor: theme.palette.mode === "light" ? "#fff" : grey[800],
 }))
 
 const TemporarySwipeableDrawer: React.FC<any> = () => {
-  const { isOpenDrawer, offDrawer, drawerComponent, openDrawer} = useBottomSwiperDrawer()
-  
+  const { isOpenDrawer, offDrawer, drawerComponent, openDrawer } =
+    useBottomSwiperDrawer()
+
   return (
     <>
       <CssBaseline />
       <Global
         styles={{
-          '.MuiDrawer-root > .MuiPaper-root': {
+          ".MuiDrawer-root > .MuiPaper-root": {
             height: `calc(50% - ${drawerBleeding}px)`,
-            overflow: 'visible',
+            overflow: "visible",
           },
         }}
       />
@@ -49,13 +50,17 @@ const TemporarySwipeableDrawer: React.FC<any> = () => {
       >
         <StyledBox
           sx={{
-            borderRadius: '10px',
-            height: '100%',
-            overflow: 'auto',
+            borderRadius: "10px",
+            height: "100%",
+            overflow: "auto",
             padding: 3,
           }}
-        > 
-          { !!drawerComponent ? drawerComponent : <Skeleton variant="rectangular" height="100%" /> }
+        >
+          {!!drawerComponent ? (
+            drawerComponent
+          ) : (
+            <Skeleton variant="rectangular" height="100%" />
+          )}
         </StyledBox>
       </SwipeableDrawer>
     </>
