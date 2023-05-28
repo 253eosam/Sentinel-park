55f1d2cbf005f66ac8a7cc9ccd26d6723c57aefa
Seongjun Lee <253eosam@gmail.com>
2023-05-29 01:09:28 +0900

    eslint husky setting test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { RecoilRoot } from 'recoil'
-import Head from 'next/head'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import { RecoilRoot } from "recoil"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main className='bg-primary pb-5'>
-    <RecoilRoot>
-      <Component  {...pageProps} />
-    </RecoilRoot>
-
+    <main className="bg-primary pb-5">
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
     </main>
   )
 }
 
 export default MyApp
-
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import AppTemplate from "@src/components/AppTemplate"
 
 const index: React.FC<any> = () => {
-
   return (
-    <> 
+    <>
       <AppTemplate></AppTemplate>
     </>
   )
